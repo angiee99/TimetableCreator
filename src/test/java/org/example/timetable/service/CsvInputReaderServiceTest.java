8817eac3227fbde506c95458550e81b8052399d1
Anhelina Kulkova <angeltutova@gmail.com>
2024-07-11 15:19:47 +0200

    Added the size control for temporary test

--- src/test/java/org/example/timetable/service/CsvInputReaderServiceTest.java
+++ src/test/java/org/example/timetable/service/CsvInputReaderServiceTest.java
@@ -1,12 +1,20 @@
 package org.example.timetable.service;
 
+import org.example.timetable.model.ScheduleElement;
 import org.example.timetable.service.implementation.CsvInputReaderService;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CsvInputReaderServiceTest {
     @Test
     void test(){
         CsvInputReaderService readerService = new CsvInputReaderService();
-        readerService.read("/Users/angelina/Documents/BachelorsWork/stage0/DemoInputSchedule.csv");
+        List<ScheduleElement> list = readerService
+                .read("/Users/angelina/Documents/BachelorsWork/stage0/DemoInputSchedule.csv");
+
+        assertEquals(10, list.size());
     }
 }
