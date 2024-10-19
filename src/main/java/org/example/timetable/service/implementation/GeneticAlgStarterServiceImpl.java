ca5cd195c17a1b6eebeb5a379d1ca37b36963792
Anhelina Kulkova <angelinkulk@gmail.com>
2024-10-19 14:34:18 +0200

    Added the check for exception after all iterations of GA

--- src/main/java/org/example/timetable/service/implementation/GeneticAlgStarterServiceImpl.java
+++ src/main/java/org/example/timetable/service/implementation/GeneticAlgStarterServiceImpl.java
@@ -4,6 +4,8 @@ import org.example.timetable.model.Activity;
 import org.example.timetable.model.Gene;
 import org.example.timetable.model.Generation;
 import org.example.timetable.model.Individual;
+import org.example.timetable.model.exception.NoFitIndividualException;
+import org.example.timetable.model.exception.NoSolutionFoundException;
 import org.example.timetable.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,23 +40,22 @@ public class GeneticAlgStarterServiceImpl implements GeneticAlgStarterService {
     }
 
     @Override
-    public List<Activity> createSchedule(List<Activity> activities) {
+    public List<Activity> createSchedule(List<Activity> activities) throws NoSolutionFoundException{
         Generation generation = populationGenerator.generate(activities, POPULATION_SIZE);
-        Generation selectedFromPreviousGeneration = generation;
+        Generation selectedFromPreviousGeneration = generation.copyWithPopulation();
 
         for (int i = 0; i < GENERATION_COUNT; i++) {
 
             ArrayList<Individual> selectedPopulation = (ArrayList<Individual>) selectionService.select(
                     List.copyOf(generation.getPopulation()));// selection
 
-            if(selectedPopulation.isEmpty()){ // no solution found
+            if(selectedPopulation.isEmpty()){ // no solution found in this iteration (think ab this case, if it breaks all the iteration)
                 generation = selectedFromPreviousGeneration.copyWithPopulation();
                 break;
             }
 
-            selectedFromPreviousGeneration = new Generation(selectedPopulation); //save the selected population
-
             // what if only 1 selected?
+            selectedFromPreviousGeneration = new Generation(selectedPopulation); //save the selected population
 
             // if fitness target is met, or it is the last iteration -> break with new selected individuals
             if(selectedFromPreviousGeneration.getBestIndividual().getFitness() <= FITNESS_TARGET
@@ -70,10 +71,21 @@ public class GeneticAlgStarterServiceImpl implements GeneticAlgStarterService {
 
             generation = new Generation(new ArrayList<>(populationWithMutations)); // update current generation
         }
+        // return if generation is empty
+        if(generation.getPopulation().isEmpty()){
+            System.out.println("No solution was found, the last generation is empty.");
+            throw new NoSolutionFoundException("No solution was found, the last generation is empty");
+        }
 
-        Individual bestIndividual = generation.getBestIndividual();
-        List<Activity> result = bestIndividual.getGenes().stream().map(Gene::getActivity).toList();
-        return result;
+        Individual bestIndividual;
+        try{
+            bestIndividual = generation.getBestIndividual();
+            List<Activity> result = bestIndividual.getGenes().stream().map(Gene::getActivity).toList();
+            return result;
+        } catch (NoFitIndividualException e){
+            System.out.println("No solution was found, exception:" + e.getLocalizedMessage());
+            throw new NoSolutionFoundException(
+                    "No solution was found, exception:" + e.getLocalizedMessage(), e);
+        }
     }
-
 }
